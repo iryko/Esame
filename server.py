47e15d716ab3b95fa4e91c82660edfe6cda96b46
irynakorda@gmail.com <irynakorda@gmail.com>
2017-11-28 12:06:01 +0100

    Added function put for Event

--- server.py
+++ server.py
@@ -6,20 +6,6 @@ app = Flask(__name__)
 api = Api(app)
 
 events = {}
-cur_id = 0
-
-# events = {
-#     "birthday_01/11/2017": {
-#         "title": "birthday",
-#         "description": "Mary's birthday",
-#         "date": "2017/10/12"
-#     },
-#     "baptism_10/12/2017": {
-#         "title": "baptism",
-#         "description": "Peter's baptism",
-#         "date": "2017/11/01"
-#     }
-# }
 
 def non_empty_str(val, name):
     if not str(val).strip():
@@ -36,7 +22,7 @@ class Events(Resource):
         args = parser.parse_args(strict=True)
 
         event_id = args['title'] + "_" + args['date']
-        events[events_id] = {
+        events[event_id] = {
             'title': args['title'],
             'description': args['description'],
             'date': args['date'],
@@ -54,8 +40,24 @@ class Event(Resource):
         else:
             return None, NOT_FOUND
 
-    #def put(self, title, date):
-    
+    def put(self, title, date):
+        event_id = title + "_" + date
+        if event_id in events:
+            parser = reqparse.RequestParser()
+            parser.add_argument('title', type=non_empty_str, required=True)
+            parser.add_argument('description', type=non_empty_str, required=True)
+            parser.add_argument('date', type=non_empty_str, required=True)
+            args = parser.parse_args(strict=True)
+
+            events[event_id] = {
+                'title': args['title'],
+                'description': args['description'],
+                'date': args['date'],
+            }
+            return events[event_id], OK
+        else:
+            return None, NOT_FOUND
+
     def delete(self, title, date):
         event_id = title + "_" + date
         if event_id in events:
